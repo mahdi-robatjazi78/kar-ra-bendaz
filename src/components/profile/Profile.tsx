e2066ce725db9c7e86118b14840c55f8b09e435d
mahdi robatjazi <realmehdi1999m@gmail.com>
2023-07-28 01:05:50 +0330

    modify table show todos

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -9,41 +9,5 @@ export default function Profile() {
   const theme = React.useContext(ThemeContext);
   const { auth } = useSelector((state) => state.auth);
 
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "85vh",
-      }}
-    >
-      <Card
-        sx={{
-          minWidth: "30rem",
-          background: theme.sidebar,
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            flexDirection: "row-reverse",
-          }}
-        >
-          <CardContent style={{ padding: 0 }}>
-            <img
-              style={{ height: "100%" }}
-              src={auth.gender === "male" ? "/male.jpg" : "/female.jpg"}
-            />
-          </CardContent>
-          <Box sx={{ pl: 1, pt: 7, color: theme.text1 }}>
-            <h3>{auth.fname}</h3>
-            <h3>{auth.lname}</h3>
-            <h3>{auth.email}</h3>
-          </Box>
-        </Box>
-      </Card>
-    </Box>
-  );
+  return <Box></Box>;
 }
